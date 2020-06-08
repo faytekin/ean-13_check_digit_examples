303a7bcdb657ce4b9e67a6e9f088248e9064c6ca
Fatih Aytekin <fatih@aytekin.me>
2020-06-09 01:33:25 +0300

    Add Javascript script

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const pttCheckDigit = (barcode) => {
+    const strBarcode = String(barcode).split('');
+    let total = 0;
+
+    strBarcode.forEach((value, key) => {
+        total += key %2 == 0 ? value * 1 : value * 3;
+    });
+
+    const result = String(barcode) + (10 - total % 10);
+    return parseInt(result);
+};
+
+const barcode = 275036569845;
+const barcodeWithCheckDigit = pttCheckDigit(barcode);
+console.log('barcodeWithCheckDigit => ', barcodeWithCheckDigit);
