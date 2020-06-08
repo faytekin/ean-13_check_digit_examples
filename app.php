11f1822cf00ae506d9fb800e388bbc24a8575135
Fatih Aytekin <fatih@aytekin.me>
2020-06-08 23:33:24 +0300

    PHP Script different way

--- app.php
+++ app.php
@@ -3,14 +3,13 @@
 function pttCheckDigit($barcode)
 {
     $arrSplit = str_split($barcode);
-    $total = [];
+    $total = 0;
 
     foreach ($arrSplit as $key => $value) {
-        $total[] = $key % 2 == 0 ? $value * 1 : $value * 3;
+        $total += $key % 2 == 0 ? $value * 1 : $value * 3;
     }
 
-    $totalSum = array_sum($total);
-    return $barcode.(10 - $totalSum % 10);
+    return $barcode.(10 - $total % 10);
 }
 
 $barcode = 275036569845;
