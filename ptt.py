390588922123d295c4239cb58c63118614137f99
Fatih Aytekin <fatih@aytekin.me>
2020-06-09 15:01:20 +0300

    Add python script

---
+++ ptt.py
@@ -0,0 +1,15 @@
+
+def pttCheckDigit(barcode):
+    arrBarcode = enumerate(str(barcode))
+    total = 0
+
+    for index, value in arrBarcode:
+        value = int(value)
+        total += index % 2 == 0 and value * 1 or value * 3
+
+    return int(str(barcode) + str(10 - total % 10))
+
+
+barcode = 275036569845
+barcodeWithCheckDigit = pttCheckDigit(barcode)
+print(barcodeWithCheckDigit)
