ba6a5e74b983730384688d5b0e50cd34a8ca2747
Fatih Aytekin <fatih@aytekin.me>
2020-06-09 16:11:30 +0300

    Add dart script

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main() {
+  const barcode = 275036569845;
+  var barcodeWithCheckDigit = pttCheckDigit(barcode);
+  print(barcodeWithCheckDigit);
+}
+
+
+int pttCheckDigit(int barcode) {
+  List arrBarcode = barcode.toString().split('');
+  int total = 0;
+
+  arrBarcode.asMap().forEach((index, value) {
+    int number = int.parse(value);
+
+    total += index %2 == 0 ? number * 1 : number * 3;
+  });
+
+  return int.parse(barcode.toString() + (10 - total % 10).toString());
+}
